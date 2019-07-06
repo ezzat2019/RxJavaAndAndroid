4e84853c3c7c7601fe8200edbb6d73761d2659e4
ezzat2019 <coezz@yahoo.com>
2019-07-06 06:32:06 +0200

    this is commit2

--- app/src/main/java/com/example/programmer/rxjavaandandroid/ServicesGentrator.java
+++ app/src/main/java/com/example/programmer/rxjavaandandroid/ServicesGentrator.java
@@ -7,6 +7,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServicesGentrator {
+    private String add;
 
     public static final String BASE_URL = "https://jsonplaceholder.typicode.com";
     private static Retrofit.Builder retrofitBuilder =
